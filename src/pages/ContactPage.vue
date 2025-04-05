19aa28fa76f576aac1b28445d17d3cb36e836897
MaxDronCode <MaxDronCode>
2025-04-05 19:28:12 +0200

    Enhance ContactPage form with improved validation rules and styling for better user experience

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="row flex flex-center items-center" style="height: 90dvh;">
-    <q-form
-      @submit.prevent="onSubmit"
-      @reset.prevent="onReset"
-      class="q-gutter-md"
-    >
+  <div class="row flex flex-center items-center" style="height: 90dvh">
+    <q-form @submit.prevent="onSubmit" @reset.prevent="onReset" class="q-gutter-md">
       <q-input
         filled
         v-model="name"
         label="Nombre *"
         hint="Introduce tu nombre completo"
         lazy-rules
-        :rules="[ val => !!val || 'Por favor introduce tu nombre' ]"
+        :rules="[(val) => !!val || 'Por favor introduce tu nombre']"
+        class="custom-input"
       />
 
       <q-input
@@ -21,9 +18,10 @@
         hint="ejemplo@dominio.com"
         lazy-rules
         :rules="[
-          val => !!val || 'Por favor introduce tu correo',
-          val => /^.+@.+\..+$/.test(val) || 'El correo electrónico no es válido'
+          (val) => !!val || 'Por favor introduce tu correo',
+          (val) => /^.+@.+\..+$/.test(val) || 'El correo electrónico no es válido',
         ]"
+        class="custom-input"
       />
 
       <q-input
@@ -34,15 +32,11 @@
         autogrow
         hint="Escribe tu consulta o comentario"
         lazy-rules
-        :rules="[
-          val => !!val || 'Por favor introduce tu mensaje'
-        ]"
+        :rules="[(val) => !!val || 'Por favor introduce tu mensaje']"
+        class="custom-input"
       />
 
-      <q-toggle
-        v-model="accept"
-        label="He leído y acepto los términos y condiciones"
-      />
+      <q-toggle v-model="accept" label="He leído y acepto los términos y condiciones" />
 
       <div>
         <q-btn label="Enviar" type="submit" color="primary" />
@@ -57,7 +51,7 @@ import { ref } from 'vue'
 import { useQuasar } from 'quasar'
 
 export default {
-  setup () {
+  setup() {
     const $q = useQuasar()
 
     const name = ref('')
@@ -65,26 +59,26 @@ export default {
     const message = ref('')
     const accept = ref(false)
 
-    function onSubmit () {
+    function onSubmit() {
       if (!accept.value) {
         $q.notify({
           color: 'red-5',
           textColor: 'white',
           icon: 'warning',
-          message: 'Debe aceptar los términos y condiciones antes de enviar'
+          message: 'Debe aceptar los términos y condiciones antes de enviar',
         })
       } else {
         $q.notify({
           color: 'green-4',
           textColor: 'white',
           icon: 'cloud_done',
-          message: `Gracias ${name.value}, nos pondremos en contacto con usted en su correo ${email.value} para resolver sus dudas a la mayor brevedad.`
+          message: `Gracias ${name.value}, nos pondremos en contacto con usted en su correo ${email.value} para resolver sus dudas a la mayor brevedad.`,
         })
         onReset()
       }
     }
 
-    function onReset () {
+    function onReset() {
       name.value = ''
       email.value = ''
       message.value = ''
@@ -97,9 +91,9 @@ export default {
       message,
       accept,
       onSubmit,
-      onReset
+      onReset,
     }
-  }
+  },
 }
 </script>
 
@@ -108,5 +102,8 @@ export default {
   padding: 15px;
   border-radius: 5px;
   box-shadow: 0 0 10px 8px rgba(0, 0, 0, 0.144);
+}
+.custom-input .q-field__label {
+  color: rgb(30, 146, 196);
 }
 </style>
